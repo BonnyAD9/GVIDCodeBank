4880ad91330a0822958aed707903da3438f2bbf2
Jakub A. Štigler <jakubstig@gmail.com>
2022-02-15 22:17:11 +0100

    Added first part of Bank-13

--- Bank-13/README.md
+++ Bank-13/README.md
@@ -5,3 +5,32 @@ Pomocí podprogramů vyřeš... Názvy souborů a dalších parametrů musí hla
 získat od uživatele (ze vstupu nebo z parametrů příkazového řádku).
 - V textových souborech jsou textově uloženy dvě databázové tabulky. V první tabulce (souboru) jsou uloženy záznamy s atributy ID, jméno (10 znaků). Ve druhé tabulce (souboru) jsou uloženy záznamy s atributy ID_uživatele, e-mail (20 znaků). V první tabulce má ID charakter primárního klíče a ID_uživatele je cizí klíč odkazující do první tabulky. Napiš podprogram, který vypíše na řádky všechny emailové adresy uživatelů se zadaným jménem (parametr podprogramu). Na každém řádku výstupu budou tyto údaje ID, jméno, e-mail.
 - V tabulce (souboru) jsou záznamy o sopkách s atributy název (20 znaků), výška (v metrech), zda je aktivní (ano/ne). Vytvoř podprogram pro výpis všech záznamů o nad/podprůměrně (parametr) vysokých ne/aktivních (parametr) sopkách.
+
+## Output
+
+### users.txt
+```
+f16a Petr
+586d Hana
+cf5a Tonda
+```
+
+### emails.txt
+```
+f16a petr.bajza@gmail.com
+586d hana.best@gmail.com
+f16a jumbo@outlook.cz
+```
+
+### Console
+```
+PS > .\bank-13 user Petr users.txt emails.txt
+petr.bajza@gmail.com
+jumbo@outlook.cz
+user Petr with id f16a has 2 email adresses
+PS > .\bank-13 user Hana users.txt emails.txt
+hana.best@gmail.com
+user Hana with id f16a has 1 email adress
+PS > .\bank-13 user Tonda users.txt emails.txt
+user Tonda with id f16a has no email adress
+```
--- Bank-13/main.c
+++ Bank-13/main.c
@@ -12,27 +12,81 @@ int getUserEmails(FILE* users, FILE* emails, FILE* out, char* user);
 
 int main(int argc, char **argv)
 {
-    return 0;
+    if (argc < 3)
+    {
+        printf("invalid number of arguments\n");
+        return EXIT_FAILURE;
+    }
+    if (strcmp(argv[1], "user") == 0)
+    {
+        if (argc != 5)
+        {
+            printf("invalid number of arguments\n");
+            return EXIT_FAILURE;
+        }
+
+        FILE* users = fopen(argv[3], "r");
+        if (!users)
+        {
+            printf("couldn't open file '%s'\n", argv[3]);
+            return EXIT_FAILURE;
+        }
+
+        FILE* emails = fopen(argv[4], "r");
+        if (!emails)
+        {
+            fclose(users);
+            printf("couldn't open file '%s'\n", argv[4]);
+            return EXIT_FAILURE;
+        }
+
+        int res = getUserEmails(users, emails, stdout, argv[2]);
+        fclose(users);
+        fclose(emails);
+
+        if (res < 0)
+        {
+            printf("failed to load emails from user '%s'\n", argv[2]);
+            return EXIT_FAILURE;
+        }
+
+        int id = res & 0x0000ffff;
+        int mailCount = res >> 16;
+        switch (mailCount)
+        {
+        case 0:
+            printf("user %s with id %4x has no email adress\n", argv[2], id);
+            break;
+        case 1:
+            printf("user %s with id %4x has 1 email adress\n", argv[2], id);
+            break;
+        default:
+            printf("user %s with id %4x has %d email adresses\n", argv[2], id, mailCount);
+            break;
+        }
+    }
+
+    return EXIT_SUCCESS;
 }
 
 int getUserEmails(FILE* users, FILE* emails, FILE* out, char* user)
 {
-    unsigned short userId;
+    unsigned int userId;
     char str[21];
 
     do
     {
-        if (fscanf(users, " %x %10s", &userId, str) != 2)
+        if (fscanf(users, " %4x %10s", &userId, str) != 2)
         {
             return -1;
         }
     }
     while (strcmp(str, user) != 0);
 
-    unsigned short id;
+    unsigned int id;
     int count = 0;
 
-    while (fscanf(emails, " %x %20s", &id, str) == 2)
+    while (fscanf(emails, " %4x %20s", &id, str) == 2)
     {
         if (id == userId)
         {
